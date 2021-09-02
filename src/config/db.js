74048862415645acbbadd0092a7a776919605b74
Jefferson Kleyton <54268299+jeffersonkleyton@users.noreply.github.com>
2021-09-02 13:08:49 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const uri =
-  "mongodb+srv://jeff:1994@cluster0.mhh1g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "";
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Banco de dados On!'))
   .catch((e) => {
